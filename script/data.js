f6b441dde3bc0c42d56ebe7e7cc691a33c8185dd
jd <jiande_goh@mymail.sutd.edu.sg>
2022-02-22 15:49:27 +0800

    Updated data.js

--- script/data.js
+++ script/data.js
@@ -15,7 +15,6 @@ let tokenId = {
     'wrapped-bitcoin':'',
     "matic-network":'',
     'pancakeswap-token':'',
-    'magic-internet-money':''
 }
 getTokenId = async () => {
     let requestUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
