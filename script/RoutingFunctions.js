3165aee77e9acdd552ca4a3c70788b41d4f71d5b
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-02 04:08:12 +0800

    1inch api is working and sync with test data

--- script/RoutingFunctions.js
+++ script/RoutingFunctions.js
@@ -1,5 +1,6 @@
 BASE_URL = "https://api.1inch.io/v4.0/"
 
+// https://api.1inch.io/v4.0/1/quote?fromTokenAddress=&toTokenAddress=&amount=
 chainID ={
     "ethereum": "1",
     "binance-smart-chain": "56",
@@ -9,7 +10,7 @@ chainID ={
     "avalanche": "43114"
 }
 
-tokenDecimal = {   //self-curated list of tokens with different decimal
+specialTokenDecimal = {   //self-curated list of tokens with different decimal
     "USD Coin":6,
     "Tether":6,
     "Dogecoin":8,
@@ -27,17 +28,39 @@ function checkPlatforms(tokenA, tokenB) {
             if (itemA == itemB) commonPlatforms.push(itemA) 
         }
     }
+    console.log(commonPlatforms)
     return commonPlatforms
 }
 
+function convertAmountToDecimals(token,amount) {
+    if (specialTokenDecimal.hasOwnProperty(token)){
+        decimalString = '0'.repeat(specialTokenDecimal[token])
+    } else {
+        decimalString = '0'.repeat(18)
+    }
+    amountInString = `${amount}${decimalString}`
+    return amountInString
+}
 
-function checkQuotes(tokenA, tokenB, amount) {
+async function getDataByChain(chainAddress, addressTokenA, addressTokenB, amountInString) {
+    data = await axios.get(`${BASE_URL}${chainAddress}/quote?fromTokenAddress=${addressTokenA}&toTokenAddress=${addressTokenB}&amount=${amountInString}`).then(res => res.data)
+    return data
+}
+
+async function checkQuotes(tokenA, tokenB, amount) {
     commonPlatforms = checkPlatforms(tokenA, tokenB)
+    promiseList = []
     for(let chainName of commonPlatforms) {
         chainAddress = chainID[chainName]
         addressTokenA = tokenJson[tokenA]['platforms'][chainName]
         addressTokenB = tokenJson[tokenB]['platforms'][chainName]
-
-        console.log(`${chainName} tokenA: ${addressTokenA}, tokenB: ${addressTokenB}`)
+        amountInString = convertAmountToDecimals(tokenA,amount)
+        promiseList.push(getDataByChain(chainAddress, addressTokenA, addressTokenB, amountInString))
+    }
+    routeList = await Promise.all(promiseList).then(values => values)
+    for (route of routeList) {
+        childElement = document.createElement('div')
+        childElement.innerHTML = `${JSON.stringify(route)}`
+        document.querySelector('#show-routes').appendChild(childElement)
     }
 }
